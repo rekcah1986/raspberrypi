e7a9909a759934e98e572f2eee5dd704ad816a14
rekcah1986 <442416294@qq.com>
2017-09-05 07:27:37 +0800

    check tv

--- checkTV.sh
+++ checkTV.sh
@@ -1,2 +1,11 @@
 # 加上sudo结果才能稳定
-sudo iwlist wlan0 scanning |grep 'ESSID:"DIRECT-D3-BRAVIA"'
+ESSID='ESSID:"DIRECT-D3-BRAVIA"' # TV wifi
+tmp=$(sudo iwlist wlan0 scanning |grep $ESSID)
+echo @$tmp@$ESSID@
+if [[ $tmp =~ $ESSID ]]
+then
+    echo findout
+else
+    echo no
+fi
+
