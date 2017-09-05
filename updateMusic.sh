e772572e1aa4818d0395352837fbbf32103ecf2b
rekcah1986 <442416294@qq.com>
2017-09-05 15:13:56 +0800

    modify update music

--- updateMusic.sh
+++ updateMusic.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 cd "$( cd "$( dirname "$0"  )" && pwd  )"
-pwd
+echo download musics...
 ID=489448787
 DIR=playlist$ID
 #rm -rf $DIR
 netease-dl playlist --id $ID
+echo make play list...
 mv $DIR/*.mp3 /home/pi/Music
+rm -rf $DIR
 find /home/pi/Music/*.mp3 > clock.lst 
+echo finish.
