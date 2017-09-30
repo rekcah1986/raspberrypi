dfe3ad315ec0f139e65562eee11d0a9327ad7d40
rekcah1986 <442416294@qq.com>
2017-10-01 07:06:41 +0800

    fix music update

--- updateMusic.sh
+++ updateMusic.sh
@@ -6,7 +6,7 @@ DIR=playlist$ID
 #rm -rf $DIR
 netease-dl playlist --id $ID
 echo make play list...
-mv $DIR/*.mp3 /home/pi/Music
+mv -n $DIR/*.mp3 /home/pi/Music
 rm -rf $DIR
 find /home/pi/Music/*.mp3 > clock.lst 
 echo finish.
