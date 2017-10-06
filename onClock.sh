7108447c1584bcaaafe10eadce4a05c44d9dce14
rekcah1986 <442416294@qq.com>
2017-10-06 19:22:09 +0800

    onNight fix

--- onClock.sh
+++ onClock.sh
@@ -1,4 +1,11 @@
 cd "$( cd "$( dirname "$0"  )" && pwd )"
-mpg123 -f 20000 -z --list clock.lst &
-sleep 15m
+lua ./checkTV.lua
+ret=$?
+if [[ $ret -eq 1 ]]
+then
+	echo "TV ON"
+else
+	mpg123 -f 20000 -z --list clock.lst &
+fi
+sleep 2m
 python weather.py
