00741f2ebe8b108bd0cf38d84a47ea9a0e3a37cf
bin仔 <442416294@qq.com>
2018-11-22 20:17:32 +0800

    say by new baidu tts api

---
+++ say.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from aip import AipSpeech
+APP_ID =  '8932722'
+API_KEY = 'og25ZUOIdMamTMP7Uqf3NPCq'
+SECRET_KEY = '3cc188a371ec6ae962c26a08f112a733'
+
+client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+text = unicode(sys.argv[1], 'gbk')
+result  = client.synthesis(text, 'zh', 1, {
+    'vol': 5,
+    'per': 4,
+})
+
+if not isinstance(result, dict):
+    with open('audio.mp3', 'wb') as f:
+        f.write(result)
+        f.close()
+        os.system('mpg123 -q audio.mp3')
+	os.remove('audio.mp3')
